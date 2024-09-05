c72b4c28912281e8f134834d4ba8336c823df5ce
IGOR-KOVAL-77777 <dvpost55@yandex.ru>
2024-09-05 11:53:57 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -3,67 +3,52 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
-    # def __len__(self):
-    #     return self.number_of_floors
-
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):  # ==
-        if not isinstance(other, House):
-            return f'{other} должен быть объектом класса House'
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
     def __lt__(self, other):  # <
-        if not isinstance(other, House):
-            return f'{other} должен быть объектом класса House'
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
     def __le__(self, other):  # <=
-        if not isinstance(other, House):
-            return f'{other} должен быть объектом класса House'
-        return self.number_of_floors <= other.number_of_floors
+        return self.__eq__(other) or self.__lt__(other)
 
     def __qt__(self, other):  # >
-        if not isinstance(other, House):
-            return f'{other} должен быть объектом класса House'
-        return self.number_of_floors > other.number_of_floors
+        return not self.__le__(other)
 
     def __qe__(self, other):  # >=
-        if not isinstance(other, House):
-            return f'{other} должен быть объектом класса House'
-        return self.number_of_floors >= other.number_of_floors
+        return not self.__lt__(other)
 
     def __ne__(self, other):  # !=
-        if not isinstance(other, House):
-            return f'{other} должен быть объектом класса House'
-        return self.number_of_floors != other.number_of_floors
+        return not self.__eq__(other)
 
-    def __add__(self, value):
-        if not isinstance(value, int):
-            return f'{value} должно быть числом'
-        self.number_of_floors += value
+    def __add__(self, value):  # h1 = h1 + 10 или h1 = h1 + h2
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
 
     def __iadd__(self, value):
-        return self + value
+        return self.__add__(value)  # h1 += 10 (метод вызывается, когда используется оператор +=)
 
     def __radd__(self, value):
-        return self + value
-
+        return self.__add__(value)  # h2 = 10 + h2 (метод вызывается, когда объект находится справа от оператора +)
+                                 
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
-# __str__
-print(h1)
-print(h2)
-# print(str(h1))
-# print(str(h2))
-
-# __len__
-# print(len(h1))
-# print(len(h2))
+print(h1)  # __str__
+print(h2)  # __str__
 
 print(h1 == h2)  # __eq__
 
